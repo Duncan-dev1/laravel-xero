92ff81d7a3ed09e26bf4cfcf729530e47452f454
Duncan-dev1 <duncanrotich12107@gmail.com>
2021-10-25 12:04:51 +0300

    bug fixes

--- src/Resources/Payitems.php
+++ src/Resources/Payitems.php
@@ -15,27 +15,27 @@ class Payitems extends Xero
 
         $result = Xero::get('payitems?'.$params);
 
-        return $result['body']['Payitems'];
+        return $result['body']['PayItems'];
     }
 
-    public function find(string $paymentId)
+    public function find(string $payItemId)
     {
-        $result = Xero::get('payments/'.$paymentId);
+        $result = Xero::get('payitems/'.$payItemId);
 
-        return $result['body']['Payments'][0];
+        return $result['body']['PayItems'][0];
     }
 
-    public function update(string $paymentId, array $data)
+    public function update(string $payItemId, array $data)
     {
-        $result = Xero::post('payments/'.$paymentId, $data);
+        $result = Xero::post('payitems/'.$payItemId, $data);
 
-        return $result['body']['Payments'][0];
+        return $result['body']['PayItems'][0];
     }
 
     public function store(array $data) 
     {
-        $result = Xero::post('payments', $data);
+        $result = Xero::post('payitems', $data);
 
-        return $result['body']['Payments'][0];
+        return $result['body']['PayItems'][0];
     }
 }
