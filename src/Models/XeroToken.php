0be5287c87a3ad14af6324b03b2e3080d56a4f2c
Duncan Kipngeno <pngenoduncan@gmail.com>
2023-03-22 12:45:31 +0300

    conflict resolution

--- src/Models/XeroToken.php
+++ src/Models/XeroToken.php
@@ -9,14 +9,4 @@ use Illuminate\Database\Eloquent\Model;
 class XeroToken extends Model
 {
     protected $guarded = [];
-
-    /**
-     * @return \Illuminate\Database\Eloquent\Casts\Attribute<Carbon, never>
-     */
-    protected function expires(): Attribute
-    {
-        return Attribute::get(
-            fn(): Carbon => $this->updated_at->addSeconds($this->expires_in)
-        );
-    }
 }
